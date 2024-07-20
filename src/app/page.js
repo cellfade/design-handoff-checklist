5c3bc852e81a8de05289d01c3b2bfffd6a2222de
Andrew Miller <andrewmiller@Andrews-Mac-mini.local>
2024-07-19 21:00:23 -0600

    changed to bento URL

--- src/app/page.js
+++ src/app/page.js
@@ -158,7 +158,7 @@ const Footer = () => (
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
         </a>
       </div>
-      <a href="https://agm.framer.website/" target="_blank" rel="noopener noreferrer" className="text-sm text-gray-600 dark:text-gray-400">
+      <a href="https://bento.me/cellfade" target="_blank" rel="noopener noreferrer" className="text-sm text-gray-600 dark:text-gray-400">
         Created by Andrew G Miller
       </a>
     </div>
